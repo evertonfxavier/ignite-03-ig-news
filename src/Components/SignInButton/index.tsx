d07a6385e090b8961212023ae0b4cbecf4708b28
Everton Xavier <evertonfxavier@gmail.com>
2021-08-22 20:46:57 -0300

    add github image

--- src/Components/SignInButton/index.tsx
+++ src/Components/SignInButton/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import { signIn, signOut, useSession } from "next-auth/client";
+
 
 import styles from "./styles.module.scss";
 
@@ -14,8 +16,13 @@ export default function SignInButton() {
       className={styles.signInButton}
       onClick={() => signOut()}
     >
-      <FaGithub color="#04d361" />
-
+      {/* <FaGithub color="#04d361" /> */}
+      <img
+        src={session.user?.image}
+        alt="Minha Figura"
+        width="24px"
+        style={{ objectFit: "cover", borderRadius: "80px", marginRight: 10 }}
+      />
       {session.user?.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
