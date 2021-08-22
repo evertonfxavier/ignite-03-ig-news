b953ddc9ef11d22021cf39995447aa659b699640
Everton Xavier <evertonfxavier@gmail.com>
2021-08-22 20:31:41 -0300

    add next auth with github

--- src/Components/SignInButton/index.tsx
+++ src/Components/SignInButton/index.tsx
@@ -1,30 +1,32 @@
-// import { signIn, signOut, useSession } from "next-auth/client";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export default function SignInButton() {
-  // const [session] = useSession();
-  const isUserLoggendIn = true;
+  const [session] = useSession();
+  console.log(session);
+  // const isUserLoggendIn = true;
 
-  // return session ? (
-  return isUserLoggendIn ? (
+  // return isUserLoggendIn ? (
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
-    //   onClick={() => signOut()}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      {/* {session.user.name} */}
-      Verttin Xavier
+
+      {session.user?.name}
+      {/* Verttin Xavier */}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
-    //   onClick={() => signIn("github")}
+      onClick={() => signIn("github")}
     >
       <FaGithub color="#eba417" />
       Sign in with GitHub
