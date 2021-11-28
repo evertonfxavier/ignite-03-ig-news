e5f20d9878b4f8943d90012ab2bdd0d24e957207
Everton Xavier <evertonfxavier@gmail.com>
2021-11-28 12:15:07 -0300

    chore: using google auth

--- src/Components/Header/SignInButton/index.tsx
+++ src/Components/Header/SignInButton/index.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export default function SignInButton() {
-  
   const [session] = useSession();
-  console.log(session);
 
   return session ? (
     <button
@@ -25,17 +23,19 @@ export default function SignInButton() {
       />
 
       {session.user?.name}
-      
+
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button
-      type="button"
-      className={styles.signInButton}
-      onClick={() => signIn("github")}
-    >
-      <FaGithub color="#eba417" />
-      Sign in with GitHub
-    </button>
+    <>
+      <button
+        type="button"
+        className={styles.signInButton}
+        onClick={() => signIn("google")}
+      >
+        <FaGoogle color="#eba417" />
+        Sign in
+      </button>
+    </>
   );
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,30 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
+//Formatos de autenticação
+//JWT (Storage)
+//Next Auth (social)
+//Cognito, Auth0
+
+//https://nextjs.org/docs/authentication
+//https://next-auth.js.org/
+
+// export default NextAuth({
+//   providers: [
+//     Providers.GitHub({
+//       //Preciso criar uma aplicação dentro do github para a parte autenticação
+//       clientId: process.env.GITTHUB_CLIENT_ID,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+//       scope: "read:user",
+//     }),
+//   ],
+// });
+
 export default NextAuth({
   providers: [
-    Providers.GitHub({
-      //Preciso criar uma aplicação dentro do github para a parte autenticação
-      clientId: process.env.GITTHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: "read:user",
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
